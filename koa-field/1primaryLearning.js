f9ff79433f05f0dcb66937d10ef47e438e8eb1e2
Hallie <49779117+hallieliu123@users.noreply.github.com>
2019-12-30 15:51:29 +0800

    Update 1primaryLearning.js

--- koa-field/1primaryLearning.js
+++ koa-field/1primaryLearning.js
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-// 学习目标   
-=======
 // 学习目标
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
 // 使用koa搭建服务器   
 // context上下文 使用 及 源码封装  
 // 中间件使用 及 源码封装   
@@ -19,10 +16,6 @@ const { log } = console;
 
 // app.listen(8080);
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
 // 2.http response类型，默认返回'text/plain'; 通过 cxt.response.type 设置返回数据类型
 /*
     const fs = require('fs');
@@ -30,11 +23,8 @@ const { log } = console;
     const app = new Koa();
     app.use((cxt,next)=>{
 
-<<<<<<< HEAD
-        // cxt.response.type = 'html'; // 实际koa可以自己判断类型  
-=======
+
         // cxt.response.type = 'html'; // 实际koa可以自己判断类型
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
         // cxt.response.body = '<h1>Hello world!!!<h1>';
 
         cxt.response.type = 'html';
@@ -59,11 +49,9 @@ const { log } = console;
 
 
 // 4.使用require('koa-static')处理静态资源
-<<<<<<< HEAD
-   /*  const Koa = require('koa');
-=======
+
   /*   const Koa = require('koa');
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
     const serve = require('koa-static');
     const path = require('path'); // node用来处理文件路径的模块
     const app = new Koa();
@@ -73,13 +61,10 @@ const { log } = console;
             cxt.response.body = '看来一切都是中间件。'
         // }
     });
-<<<<<<< HEAD
+
     app.listen(8080,()=>{
         console.log('listening');
     }); */
-=======
-    app.listen(8080); */
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
 
 // 5.使用cxt.response.redirect('/路径') 重定向
     /* const Koa = require('koa');
@@ -95,11 +80,9 @@ const { log } = console;
     app.listen(8080); */
 // 6.中间件,因为它处在 http request 和 http response 之间来实现某种功能,所以叫做中间件;
 // ***多个中间件会形成一个栈结构，以先进后出(first-in-last-out)的顺序执行；
-<<<<<<< HEAD
-   /*const Koa = require('koa');
-=======
+
    /*  const Koa = require('koa');
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
     const app = new Koa();
     app.use((cxt,next)=>{
         log(cxt.request);
@@ -153,11 +136,8 @@ const { log } = console;
     app.listen(8080,()=>{
         log('listening...');
     }); */
-<<<<<<< HEAD
-// ***中间件合成 koa-compose 模块
-=======
 // ***中间件合成 koa-compose模块
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
     /* const Koa = require('koa');
     const compose = require('koa-compose'); 
     const app = new Koa();
@@ -192,20 +172,13 @@ const { log } = console;
         log('listening !!!');
     }); */
     
-<<<<<<< HEAD
-// 常见状态码 13 个
+// 常见状态码 14 个
 /**
  * 200 请求成功返回 ok
  * 302 暂时重定向
  * 304 后端数据未修改,使用浏览器缓存
-=======
-// 常见状态码 14 个
-/**
- * 200 请求成功返回 ok
- * 304 后端数据未修改,使用浏览器缓存
  * 307 暂时重定向
  * 308 永久重定向
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
  * 400 bad request 请求报文中有错误(前端参数有错误)
  * 401 unauthorized 未授权访问
  * 403 forbidden 禁止访问,请求访问的资源被服务器拒绝了
@@ -263,16 +236,13 @@ const { log } = console;
         log('listening...');
     }); */
 // 10.koa-router模块化开发
-   /*  const Koa = require('koa');
+/*  const Koa = require('koa');
     const app = new Koa();
     const Router = require('koa-router');
     const user = require('./user');
     const route = new Router();
-<<<<<<< HEAD
-    app.use(route.routes()); // 注册路由，返回一个匹配请求的路由中间件     
-=======
     app.use(route.routes()); // 注册路由，返回一个匹配请求的路由中间件
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
     app.use(route.allowedMethods()) // 返回一个中间件，如果请求方式有错误，抛出错误
     route.use('/user',user.routes());   
     app.listen(8080,()=>{
@@ -303,13 +273,9 @@ const { log } = console;
     //     ctx.response.body = 'blank';
     // });
 
-<<<<<<< HEAD
-    app.use(route.routes());   
-    app.use(route.allowedMethods());   
-=======
     app.use(route.routes());
     app.use(route.allowedMethods());
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
+
     route.get('/',async (ctx,next)=>{
         const file = await fs.readFile('./index.html');
         ctx.response.type = 'text/html';
@@ -321,11 +287,7 @@ const { log } = console;
     });
  */
 // 12.kao-body实现文件上传
-<<<<<<< HEAD
-   /*  const Koa = require('koa');
-=======
     const Koa = require('koa');
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
     const app = new Koa();
     const Router = require('koa-router');
     const route = new Router();
@@ -344,12 +306,3 @@ const { log } = console;
     app.listen(8080,()=>{
         log('listening...');
     });
-<<<<<<< HEAD
- */
-
-
-
-    
-=======
-
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
