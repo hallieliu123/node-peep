b8ed97088dfacf1fdd54160b88a9951925e67882
yangyliu <yangliu@ebay.com>
2020-01-06 16:38:26 +0800

    fix misoperation

--- koa-field/user.js
+++ koa-field/user.js
@@ -3,17 +3,10 @@ const fs = require('fs.promised');
 const route = new Router();
 const url = require('url');
 
-<<<<<<< HEAD
-route.get('/login',(ctx,next)=>{  
-    console.log(ctx.request);  
-    console.log('url-->',url.parse(ctx.request.url));  
-    cxt.response.body = 'test route';  
-=======
 route.get('/login',(ctx,next)=>{
     console.log(ctx.request);
     console.log('url-->',url.parse(ctx.request.url));
     cxt.response.body = 'test route';
->>>>>>> 7fbe72b5f707ba5474dbe42e50683ae7a3f20688
 });
 route.post('/upload/avatar',async ctx=>{
     const files = ctx.request.files || {};
